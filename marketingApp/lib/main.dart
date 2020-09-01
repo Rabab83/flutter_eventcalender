9d7c656a6e6ea0c8c554a474c54d0486b5c56d2d
rabab ebrahim <rababebrahim62@gmail.com>
2020-09-01 11:05:35 +0200

    marketing app

--- marketingApp/lib/main.dart
+++ marketingApp/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marketingApp/screens/calender.dart';
 import 'package:marketingApp/ui/pages/clientpage.dart';
+import 'package:marketingApp/widgets/auth/auth_form.dart';
 import 'widgets/raisedButton.dart';
 import 'screens/clients.dart';
 import 'package:marketingApp/ui/pages/add_event.dart';
@@ -35,7 +36,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -49,42 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Button(
-              text: Text('Employee'),
-              navigateTo: StreamBuilder(
-                stream: FirebaseAuth.instance.authStateChanges(),
-                builder: (ctx, userSnapshot) {
-                  if (userSnapshot.hasData) {
-                    return Calender();
-                  }
-                  return AuthScreen();
-                },
-              ),
-              color: Colors.pinkAccent,
-              textColor: Colors.black,
-            ),
-            Button(
-              text: Text('Client'),
-              navigateTo: StreamBuilder(
-                stream: FirebaseAuth.instance.authStateChanges(),
-                builder: (ctx, userSnapshot) {
-                  if (userSnapshot.hasData) {
-                    return ClientPage();
-                  }
-                  return AuthScreen();
-                },
-              ),
-              color: Colors.pinkAccent,
-              textColor: Colors.black,
-            ),
-          ],
-        ),
-      ),
+      body:AuthScreen(),
     );
   }
 }
